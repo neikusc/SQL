72979af0fdede12b29e34542105c45626fc685b2
neikusc <trongkien0402@gmail.com>
2014-05-31 10:57:16 -0700

    Update load_avs.sql

--- load_avs.sql
+++ load_avs.sql
@@ -1,4 +1,3 @@
-/*
 CREATE TABLE offers (
   offer int,
   category int,
@@ -7,9 +6,7 @@ CREATE TABLE offers (
   offervalue real,
   brand int  
 );
-*/
 
-/*
 CREATE TABLE trainHistory (
   id integer,
   chain integer,
@@ -19,9 +16,7 @@ CREATE TABLE trainHistory (
   repeater char,
   offerdate date
 );
-*/
 
-/*
 CREATE TABLE testHistory (
   id integer,
   chain integer,
@@ -29,9 +24,7 @@ CREATE TABLE testHistory (
   market integer,
   offerdate date
 );
-*/
 
-/*
 CREATE TABLE transactions (
   id integer,
   chain integer,
@@ -45,36 +38,27 @@ CREATE TABLE transactions (
   purchaseQuantity integer,
   purchaseAmount real
 );
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/transactions.csv' INTO TABLE transactions
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (id, chain, dept, category, company, brand, date, productsize, productmeasure, purchaseQuantity, purchaseAmount);
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/offers.csv' INTO TABLE offers
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (offer, category, quantity, company, offervalue, brand);
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/trainHistory.csv' INTO TABLE trainHistory
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (id, chain, offer, market, repeattrips, repeater, offerdate);
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/testHistory.csv' INTO TABLE testHistory
   FIELDS TERMINATED BY ','
   LINES TERMINATED BY '\n'
   IGNORE 1 LINES
   (id, chain, offer, market, offerdate);
-*/
