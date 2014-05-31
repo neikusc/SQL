25953f1ac0498818c6668f2edead79a60f7e1479
neikusc <trongkien0402@gmail.com>
2014-05-31 10:58:09 -0700

    Update load_MovieLens.sql

--- load_MovieLens.sql
+++ load_MovieLens.sql
@@ -1,4 +1,3 @@
-/*
 CREATE TABLE user (
    UserID integer,
    Gender varchar,
@@ -6,44 +5,34 @@ CREATE TABLE user (
    Occupation integer,
    Zipcode integer
 );
-*/
 
-/*
 CREATE TABLE rating (
   UserID integer,
   MovieID integer,
   Rating integer,
   Timestamp integer
 ); 
-*/
 
-/*
 CREATE TABLE movie (
   MovieID integer,
   Title varchar,
   Genres varchar
 );
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/users.dat' INTO TABLE user
    FIELDS TERMINATED BY '::'
    LINES TERMINATED BY '\n' 
    (UserID, Gender, Age, Occupation, Zipcode);
-*/
-/*
+
 LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/ratings.dat' INTO TABLE rating
    FIELDS TERMINATED BY '::'
    LINES TERMINATED BY '\n' 
    (UserID, MovieID, Rating, Timestamp);
-*/
 
-/*
 LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/movies.dat' INTO TABLE movie
    FIELDS TERMINATED BY '::'
    LINES TERMINATED BY '\n' 
    (MovieID, Title, Genres);
-*/
 
 SELECT * INTO OUTFILE '/tmp/movies.csv'
   FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
