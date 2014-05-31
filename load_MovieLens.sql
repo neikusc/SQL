ec39d9031ec17031ad0475b1f2741207824c2c99
neikusc <trongkien0402@gmail.com>
2014-05-30 17:45:03 -0700

    multiple projects

---
+++ load_MovieLens.sql
@@ -0,0 +1,52 @@
+/*
+CREATE TABLE user (
+   UserID integer,
+   Gender text,
+   Age integer,
+   Occupation integer,
+   Zipcode integer
+);
+*/
+
+/*
+CREATE TABLE rating (
+  UserID integer,
+  MovieID integer,
+  Rating integer,
+  Timestamp integer
+); 
+*/
+
+/*
+CREATE TABLE movie (
+  MovieID integer,
+  Title text,
+  Genres text
+);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/users.dat' INTO TABLE user
+   FIELDS TERMINATED BY '::'
+   LINES TERMINATED BY '\n' 
+   (UserID, Gender, Age, Occupation, Zipcode);
+*/
+/*
+LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/ratings.dat' INTO TABLE rating
+   FIELDS TERMINATED BY '::'
+   LINES TERMINATED BY '\n' 
+   (UserID, MovieID, Rating, Timestamp);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/DataScience/MovieLens/movies.dat' INTO TABLE movie
+   FIELDS TERMINATED BY '::'
+   LINES TERMINATED BY '\n' 
+   (MovieID, Title, Genres);
+*/
+
+SELECT * INTO OUTFILE '/tmp/movies.csv'
+  FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+  LINES TERMINATED BY '\n'
+  FROM movie;
+
---
+++ load_avs.sql
@@ -0,0 +1,80 @@
+/*
+CREATE TABLE offers (
+  offer int,
+  category int,
+  quantity int,
+  company int,
+  offervalue real,
+  brand int  
+);
+*/
+
+/*
+CREATE TABLE trainHistory (
+  id integer,
+  chain integer,
+  offer integer,
+  market integer,
+  repeattrips integer,
+  repeater char,
+  offerdate date
+);
+*/
+
+/*
+CREATE TABLE testHistory (
+  id integer,
+  chain integer,
+  offer integer,
+  market integer,
+  offerdate date
+);
+*/
+
+/*
+CREATE TABLE transactions (
+  id integer,
+  chain integer,
+  dept integer,
+  category integer,
+  company integer,
+  brand integer,
+  date date,
+  productsize integer,
+  productmeasure char,
+  purchaseQuantity integer,
+  purchaseAmount real
+);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/transactions.csv' INTO TABLE transactions
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (id, chain, dept, category, company, brand, date, productsize, productmeasure, purchaseQuantity, purchaseAmount);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/offers.csv' INTO TABLE offers
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (offer, category, quantity, company, offervalue, brand);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/trainHistory.csv' INTO TABLE trainHistory
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (id, chain, offer, market, repeattrips, repeater, offerdate);
+*/
+
+/*
+LOAD DATA LOCAL INFILE '/home/neik/Kaggle/AcquireValuedShoppers/CSV/testHistory.csv' INTO TABLE testHistory
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  IGNORE 1 LINES
+  (id, chain, offer, market, offerdate);
+*/
